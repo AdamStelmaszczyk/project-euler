a21e4e355adfa19a1542d25a022d24bca522eb5d
Adam Stelmaszczyk <stelmaszczykadam@gmail.com>
2017-03-28 19:17:13 +0100

    Add PROBLEM n headers

--- solutions.hs
+++ solutions.hs
@@ -1,3 +1,5 @@
+-- PROBLEM 1
+
 -- O(n)
 problem_1 :: Integer -> Integer
 problem_1 m = sum [n | n <- [1..m-1], n `mod` 3 == 0 || n `mod` 5 == 0]
@@ -11,6 +13,9 @@ divisors_sum m d = d * (l + 1) * l `div` 2
 problem_1' :: Integer -> Integer
 problem_1' m = divisors 3 + divisors 5 - divisors 15
   where divisors = divisors_sum m
+
+
+-- PROBLEM 2
 
 -- n-th Fibonacci number 1-indexed: 1, 1, 2, 3, 5, 8, ...
 -- O(phi^n), where phi is the golden ratio (1.618...)
