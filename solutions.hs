c68c892d7448f1ee9c85d8579401a8898dd208b2
Adam Stelmaszczyk <stelmaszczykadam@gmail.com>
2017-03-28 19:18:34 +0100

    Change m to n for consitency

--- solutions.hs
+++ solutions.hs
@@ -2,17 +2,17 @@
 
 -- O(n)
 problem_1 :: Integer -> Integer
-problem_1 m = sum [n | n <- [1..m-1], n `mod` 3 == 0 || n `mod` 5 == 0]
+problem_1 n = sum [d | d <- [1..n-1], d `mod` 3 == 0 || d `mod` 5 == 0]
 
--- Sum of positive integers divisible by d smaller than m
+-- Sum of positive integers divisible by d smaller than n
 divisors_sum :: Integer -> Integer -> Integer
-divisors_sum m d = d * (l + 1) * l `div` 2
-  where l = (m - 1) `div` d
+divisors_sum n d = d * (l + 1) * l `div` 2
+  where l = (n - 1) `div` d
 
 -- O(1)
 problem_1' :: Integer -> Integer
-problem_1' m = divisors 3 + divisors 5 - divisors 15
-  where divisors = divisors_sum m
+problem_1' n = divisors 3 + divisors 5 - divisors 15
+  where divisors = divisors_sum n
 
 
 -- PROBLEM 2
