9b2d8d046cbda7c60b60d90e9ca5e01e9543b204
Adam Stelmaszczyk <stelmaszczykadam@gmail.com>
2017-03-29 15:06:05 +0100

    Use camelCase instead of snake_case

--- solutions.hs
+++ solutions.hs
@@ -1,18 +1,18 @@
 -- PROBLEM 1
 
 -- O(n)
-problem_1 :: Integer -> Integer
-problem_1 n = sum [d | d <- [1..n-1], d `mod` 3 == 0 || d `mod` 5 == 0]
+problem1 :: Integer -> Integer
+problem1 n = sum [d | d <- [1..n-1], d `mod` 3 == 0 || d `mod` 5 == 0]
 
 -- Sum of positive integers divisible by d smaller than n
-divisors_sum :: Integer -> Integer -> Integer
-divisors_sum n d = d * (l + 1) * l `div` 2
+divisorsSum :: Integer -> Integer -> Integer
+divisorsSum n d = d * (l + 1) * l `div` 2
   where l = (n - 1) `div` d
 
 -- O(1)
-problem_1' :: Integer -> Integer
-problem_1' n = divisors 3 + divisors 5 - divisors 15
-  where divisors = divisors_sum n
+problem1' :: Integer -> Integer
+problem1' n = divisors 3 + divisors 5 - divisors 15
+  where divisors = divisorsSum n
 
 
 -- PROBLEM 2
@@ -38,17 +38,17 @@ fibs'' :: [Integer]
 fibs'' = map fst (iterate (\(a, b) -> (b, a + b)) (1, 1))
 
 -- O(n)
-problem_2 :: Integer -> Integer
-problem_2 n = sum (takeWhile (<= n) (filter even fibs''))
+problem2 :: Integer -> Integer
+problem2 n = sum (takeWhile (<= n) (filter even fibs''))
 
 -- Stream of even Fibonacci numbers (every 3rd): 2, 8, 34, 144, ...
 -- O(n)
-even_fibs :: [Integer]
-even_fibs = map fst (iterate (\(a, b) -> (b, 4*b + a)) (2, 8))
+evenFibs :: [Integer]
+evenFibs = map fst (iterate (\(a, b) -> (b, 4*b + a)) (2, 8))
 
 -- O(n)
-problem_2' :: Integer -> Integer
-problem_2' n = sum (takeWhile (<= n) even_fibs)
+problem2' :: Integer -> Integer
+problem2' n = sum (takeWhile (<= n) evenFibs)
 
 
 -- PROBLEM 3
@@ -57,8 +57,8 @@ problem_2' n = sum (takeWhile (<= n) even_fibs)
 -- After d = 2, increasing d by 2 instead of 1 is possible.
 -- I liked the code brevity without this though.
 -- It also doesn't change the complexity.
-problem_3 :: Integer -> Integer
-problem_3 n = divide n 2
+problem3 :: Integer -> Integer
+problem3 n = divide n 2
               where divide n d | n <= 1                             = error "n <= 1 has no prime factors"
                                | n == d                             = d
                                | n `mod` d == 0                     = divide (n `div` d) d
