0867fdf964f9cab9df5cb57f455f6276f8d27d6a
Adam Stelmaszczyk <stelmaszczykadam@gmail.com>
2017-06-30 00:54:54 +0200

    Formatting, x * x instead of x*x

--- solutions.hs
+++ solutions.hs
@@ -7,7 +7,7 @@ import qualified Data.Map.Strict as Map
 sieve xs = sieve' xs Map.empty
            where sieve' []     table = []
                  sieve' (x:xs) table = case Map.lookup x table of
-                                       Nothing    -> x : sieve' xs (Map.insert (x*x) [x] table)
+                                       Nothing    -> x : sieve' xs (Map.insert (x * x) [x] table)
                                        Just facts -> sieve' xs (foldl' reinsert (Map.delete x table) facts)
                                                      where reinsert table prime = Map.insertWith (++) (x + prime) [prime] table
 
