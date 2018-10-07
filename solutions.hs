323d8072b463d82f285503941f314f196c17c1ee
Adam Stelmaszczyk <stelmaszczykadam@gmail.com>
2018-10-07 21:49:25 +0200

    Problem 11

--- solutions.hs
+++ solutions.hs
@@ -1,15 +1,15 @@
-import           Data.List
-import           Data.Map.Strict (Map, (!))
-import qualified Data.Map.Strict as Map
+import qualified Data.List as L
+import qualified Data.Map.Strict as M
+import qualified Data.Array as A
 
 -- From "The Genuine Sieve of Eratosthenes" by Melissa O'Neill.
 -- O(n * log n * log log n)
-sieve xs = sieve' xs Map.empty
+sieve xs = sieve' xs M.empty
            where sieve' []     table = []
-                 sieve' (x:xs) table = case Map.lookup x table of
-                                       Nothing    -> x : sieve' xs (Map.insert (x * x) [x] table)
-                                       Just facts -> sieve' xs (foldl' reinsert (Map.delete x table) facts)
-                                                     where reinsert table prime = Map.insertWith (++) (x + prime) [prime] table
+                 sieve' (x:xs) table = case M.lookup x table of
+                                       Nothing    -> x : sieve' xs (M.insert (x * x) [x] table)
+                                       Just facts -> sieve' xs (L.foldl' reinsert (M.delete x table) facts)
+                                                     where reinsert table prime = M.insertWith (++) (x + prime) [prime] table
 
 -- Stream of n prime numbers: 2, 3, 5, 7, 11, ...
 -- O(n * log n * log log n)
@@ -189,17 +189,70 @@ problem10 n = sum (takeWhile (<n) primes)
 -- (0.14 secs, 56081600 bytes)
 -- floor (2000000 ** 0.75) == 53182
 problem10' :: Integer -> Integer
-problem10' n = (lucy (Map.fromList [(i, i * (i + 1) `div` 2 - 1) | i <- vs]) 2 r vs) ! n
+problem10' n = (lucy (M.fromList [(i, i * (i + 1) `div` 2 - 1) | i <- vs]) 2 r vs) M.! n
                where vs = [n `div` i | i <- [1..r]] ++ [n', n' - 1 .. 1]
                      r  = floor (sqrt (fromIntegral n))
                      n' = n `div` r - 1
 
-lucy :: Map Integer Integer -> Integer -> Integer -> [Integer] -> Map Integer Integer
+lucy :: M.Map Integer Integer -> Integer -> Integer -> [Integer] -> M.Map Integer Integer
 lucy m p r vs | p > r               = m
-              | m ! p > m ! (p - 1) = lucy (update m vs p) (p + 1) r vs
+              | m M.! p > m M.! (p - 1) = lucy (update m vs p) (p + 1) r vs
               | otherwise           = lucy m (p + 1) r vs
 
-update :: Map Integer Integer -> [Integer] -> Integer -> Map Integer Integer
-update m v p = foldl' decrease m (takeWhile (>= p*p) v)
-               where decrease m v = Map.adjust (subtract (sumOfSieved v)) v m
-                     sumOfSieved v = p * (m ! (v `div` p) - m ! (p - 1))
+update :: M.Map Integer Integer -> [Integer] -> Integer -> M.Map Integer Integer
+update m v p = L.foldl' decrease m (takeWhile (>= p*p) v)
+               where decrease m v = M.adjust (subtract (sumOfSieved v)) v m
+                     sumOfSieved v = p * (m M.! (v `div` p) - m M.! (p - 1))
+
+
+-- PROBLEM 11
+
+problem11numbers = [
+    08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 08,
+    49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00,
+    81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65,
+    52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91,
+    22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80,
+    24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50,
+    32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70,
+    67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 08, 40, 91, 66, 49, 94, 21,
+    24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72,
+    21, 36, 23, 09, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95,
+    78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 09, 53, 56, 92,
+    16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57,
+    86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58,
+    19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40,
+    04, 52, 08, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66,
+    88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69,
+    04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 08, 46, 29, 32, 40, 62, 76, 36,
+    20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16,
+    20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54,
+    01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48]
+
+problem11grid :: A.Array (Int, Int) Int
+problem11grid = A.listArray ((0, 0), (19, 19)) problem11numbers
+
+xy :: Int -> Int -> Int
+xy x y = problem11grid A.! (x, y)
+
+-- O(n^2), where n is the grid side
+-- > problem11
+-- 70600674
+-- (0.04 secs, 8,627,808 bytes)
+problem11 :: Int
+problem11 = maximum $ concat [horizontal, vertical, diagonal1, diagonal2]
+
+productOf4 :: Int -> Int -> Int -> Int -> Int
+productOf4 x dx y dy = product [xy (x + dx * i) (y + dy * i) | i <- [0..3]]
+
+horizontal :: [Int]
+horizontal = [productOf4 x 1 y 0 | x <- [0..16], y <- [0..19]]
+
+vertical :: [Int]
+vertical = [productOf4 x 0 y 1 | x <- [0..19], y <- [0..16]]
+
+diagonal1 :: [Int]
+diagonal1 = [productOf4 x 1 y 1 | x <- [0..16], y <- [0..16]]
+
+diagonal2 :: [Int]
+diagonal2 = [productOf4 (x + 3) (-1) y 1 | x <- [0..16], y <- [0..16]]
